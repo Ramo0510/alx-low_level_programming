acccc1fe48796aacce7c243d803263ef844aa181
Ramo0510 <omardij01@gmail.com>
2023-03-18 13:41:15 -0700

    Lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+char alphabet = 'z';
+
+while (alphabet >= 'a')
+{
+putchar(alphabet);
+alphabet--;
+}
+putchar('\n');
+
+return (0);
+}
